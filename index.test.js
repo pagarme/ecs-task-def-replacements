be7983a042933cb306d0dfb02bca838ca8b04f56
Fabiano Amaral Freitas <fabianin.amaral@gmail.com>
2021-09-21 17:59:22 -0300

    feat: add suport for ECS Tasks

--- index.test.js
+++ index.test.js
@@ -1,13 +1,6 @@
-const wait = require('./wait');
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-});
 
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
+test('dummy test', async () => {
+  await expect(true).to.be(true)
+})
+
